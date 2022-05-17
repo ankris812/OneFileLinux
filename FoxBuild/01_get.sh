bd86150ff26883fd96742fcb9a71bef08b72e095
ankris812 <62661743+ankris812@users.noreply.github.com>
2022-05-17 12:34:44 +0200

    Update 01_get.sh

--- FoxBuild/01_get.sh
+++ FoxBuild/01_get.sh
@@ -1,12 +1,9 @@
 #/bin/bash
 
-alpineminirootfsfile="alpine-minirootfs-3.11.6-x86_64.tar.gz"
-linuxver="linux-5.6.8"
-
-wget -4 http://dl-cdn.alpinelinux.org/alpine/v3.11/releases/x86_64/$alpineminirootfsfile
+wget -4 http://dl-cdn.alpinelinux.org/alpine/v3.15/releases/x86_64/alpine-minirootfs-3.15.4-x86_64.tar.gz
 mkdir alpine-minirootfs
-tar -C ./alpine-minirootfs -xf $alpineminirootfsfile
-wget -4 https://cdn.kernel.org/pub/linux/kernel/v5.x/$linuxver.tar.xz
-tar -xf $linuxver.tar.xz
+tar -C ./alpine-minirootfs -xf alpine-minirootfs-3.15.4-x86_64.tar.gz
+wget -4 https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.40.tar.xz
+tar -xf linux-5.15.40.tar.xz
 
-ln -s $linuxver linux
+ln -s linux-5.15.40 linux
